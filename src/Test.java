c424d62bdcb52a958b03fc712246d79ee871619e
TejasTT <tejastemghare007@gmail.com>
2022-03-05 12:44:38 +0530

    First Message

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hii");
+    }
+}
+//git remote add origin
+// git init
+
+// git add. - add all root files(Staging area)
+// git add -A add all files.
+// git add. - to add specific file give path
+//
+//git commit -m "1st commit"
+// git push origin(branch name) or master
+
+
+// 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
